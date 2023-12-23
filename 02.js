68c6d1e0575de498a8a4c1c82ded03c75da3735a
Deshraj Singh <117107054+Deshraj626844@users.noreply.github.com>
2023-12-23 10:53:54 +0000

    doneee

---
+++ 02.js
@@ -0,0 +1,8 @@
+// Variables and constant 
+const accountId=14453;   // cannot be changed 
+let accountEmail="rajveer123@gmail.com";  
+var accountPassword="12345";
+
+// Prefer not to use var because of issue in block scope and functional scope .
+
+console.table([accountId,accountEmail,accountPassword]);
