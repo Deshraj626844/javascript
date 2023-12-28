abb83dbfa5c59932328e0ad90daf9de6c316590c
Deshraj Singh <117107054+Deshraj626844@users.noreply.github.com>
2023-12-28 17:36:27 +0000

    Array methods

--- 12.js
+++ 12.js
@@ -28,3 +28,17 @@ map.set('IN',"India");
 map.set('NZ',"New Zealand");
 map.set('Fr',"France");
 console.log(map);
+
+for (const [key,value] of map) {
+   console.log(key,':-',value);
+}
+
+
+const myObject= {
+   'game1':'NFS',
+   'game2':'Spiderman'
+}
+
+for (const [key,value] of myObject) {
+   console.log(key,':-',value);
+}
---
+++ 13.js
@@ -0,0 +1,9 @@
+const myObject={
+    js:"javascript",
+    cpp:"C++",
+    rb:"ruby"
+}
+
+for (const key in myObject) {
+    console.log(myObject[key]);
+}
